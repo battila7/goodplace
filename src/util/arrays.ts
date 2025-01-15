62145dd32bb1f0df38384ef1ba0d5947c5804898
Attila Bagossy <bagossyattila@outlook.com>
2025-01-15 11:51:49 +0100

    refactor: cleaned up sights

---
+++ src/util/arrays.ts
@@ -0,0 +1,13 @@
+export function chunks<T>(array: T[], chunkCount: number): T[][] {
+    const results: T[][] = []
+    for (let i = 0; i < chunkCount; i++) {
+        results.push([])
+    }
+
+    let counter = 0;
+    while (array.length) {
+        results[counter % chunkCount].push(array.shift()!)
+        counter++
+    }
+    return results
+}
