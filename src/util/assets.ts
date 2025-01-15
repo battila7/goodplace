9438291ee8754ce3e929783d36ca25d894eb0b5b
Attila Bagossy <bagossyattila@outlook.com>
2025-01-15 13:21:32 +0100

    feat: loading raw svgs

--- src/util/assets.ts
+++ src/util/assets.ts
@@ -2,9 +2,16 @@ import type { ImageMetadata } from "astro"
 
 const decapImages = import.meta.glob<{ default: ImageMetadata }>('/src/assets/images/*.{jpeg,jpg,png,gif}')
 
+const decapSvgs = import.meta.glob<{ default: string }>('/src/assets/images/*.svg', { query: 'raw' })
+
 const Assets = {
     getImage(relativePath: string) {
         return decapImages["/" + relativePath]()
+    },
+    async getSvg(relativePath: string) {
+        const value = await decapSvgs["/" + relativePath]()
+
+        return value.default
     }
 }
 
