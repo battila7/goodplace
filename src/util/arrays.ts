6bdba5d99cfee1c0ed902cda180450f66b6c75a8
Attila Bagossy <bagossyattila@outlook.com>
2025-03-10 13:01:22 +0100

    feat: proper gallery buttons

--- src/util/arrays.ts
+++ src/util/arrays.ts
@@ -12,3 +12,25 @@ export function chunks<T>(array: T[], chunkCount: number): T[][] {
     return results
 }
 
+interface ItemAndIndex<T> {
+    item: T
+    index: number
+}
+
+export function chunksAndIndices<T>(array: T[], chunkCount: number): ItemAndIndex<T>[][] {
+    const results: ItemAndIndex<T>[][] = []
+    for (let i = 0; i < chunkCount; i++) {
+        results.push([])
+    }
+
+    let counter = 0;
+    while (array.length) {
+        results[counter % chunkCount].push({
+            item: array.shift()!,
+            index: counter
+        })
+        counter++
+    }
+    return results
+}
+
